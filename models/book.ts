d0f933d85636ca69d438be4b181850db34c96ef1
Elias Peteri <elias.peteri@hotmail.com>
2022-01-31 16:31:40 +0200

    Added "whenCreated" to BookSchema.
    Also changed description field to optional here.

--- models/book.ts
+++ models/book.ts
@@ -5,15 +5,17 @@ import { Book } from '../types';
 
 export interface IBook extends Document {
   author: string;
-  description: string;
+  description?: string;
   title: string;
+  whenCreated: Date;
 }
 
-const bookSchema: Schema = new Schema<Book>({
+const BookSchema: Schema = new Schema<Book>({
   author: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
+    unique: true
   },
   description: {
     type: String,
@@ -23,15 +25,19 @@ const bookSchema: Schema = new Schema<Book>({
     type: String,
     required: true,
     minlength: 3
+  },
+  whenCreated: {
+    type: Date,
+    required: true
   }
 });
 
-bookSchema.set('toJSON', {
-  transform: (_document, returnedObject) => {
+BookSchema.set('toJSON', {
+  transform: (_document, returnedObject): void => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
   }
 });
 
-export default model<IBook>('Book', bookSchema);
+export default model<IBook>('Book', BookSchema);
